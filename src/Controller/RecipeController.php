35f7a9b56b56507c83621e9c30cc6789b57ca9ee
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2024-10-16 17:11:08 +0200

    Add portion calculation with query parameter

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -4,14 +4,22 @@ namespace App\Controller;
 
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
     #[Route('/recipe/{id}', name: 'app_recipe_show')]
-    public function show(int $id, RecipeRepository $recipeRepository ) : Response 
+    public function show(int $id, RecipeRepository $recipeRepository, Request $request) : Response 
     {
+        $servings = $request->query->get('servings', '4');
+
+        if (+$servings < 1) {
+            throw new BadRequestHttpException('Portionen müssen mindestens 1 sein.');
+        }
+        
         $recipe = $recipeRepository->find($id);
         if(!$recipe) {
             throw $this->createNotFoundException('Recipe not found');
@@ -19,6 +27,7 @@ class RecipeController extends AbstractController
 
         return $this->render('recipe/show.html.twig', [
             'recipe' => $recipe,
+            'servings' => $servings,
         ]);
     }
 }
