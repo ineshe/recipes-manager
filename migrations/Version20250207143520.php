19b821f6c3015c0d05e94faa9793aac3aa75cf84
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-02-07 17:32:57 +0100

    Rename column

--- migrations/Version20250207143520.php
+++ migrations/Version20250207143520.php
@@ -19,8 +19,8 @@ final class Version20250207143520 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('UPDATE recipe SET image_path = SUBSTRING_INDEX(image_path, "/", -1)');
-        $this->addSql('UPDATE category SET image_path = SUBSTRING_INDEX(image_path, "/", -1)');
+        $this->addSql('UPDATE recipe SET image = SUBSTRING_INDEX(image, "/", -1)');
+        $this->addSql('UPDATE category SET image = SUBSTRING_INDEX(image, "/", -1)');
     }
 
     public function down(Schema $schema): void
