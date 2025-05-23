416003cb16881ed86787835dd3f3114b5b85c443
Ines Heilmann <27891081+ineshe@users.noreply.github.com>
2025-05-23 16:18:48 +0200

    Set default values for new steps in administration

--- assets/js/recipe_defaults.js
+++ assets/js/recipe_defaults.js
@@ -1,14 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.addEventListener('change', event => {
-        if (!event.target.matches('select[name$="[recipeIngredient]"]')) return;
-        const select = event.target;
-        
+    function addDefaultsForSelect(select) {
         const prefix = select.name.replace(/\[recipeIngredient\]$/, '');
         const amountInput = document.querySelector(`input[name="${prefix}[amount]"]`);
         const unitInput = document.querySelector(`input[name="${prefix}[unit]"]`);
 
         const { amount = '', unit = '' } = select.selectedOptions[0].dataset;
         amountInput.value = amount;
-        unitInput.value   = unit;
+        unitInput.value   = unit;        
+    };
+
+    document.body.addEventListener('change', event => {
+        if (!event.target.matches('select[name$="[recipeIngredient]"]')) return;
+        addDefaultsForSelect(event.target);
+    });
+
+    const observer = new MutationObserver(mutations => {
+        for (const { addedNodes } of mutations) {
+            for (const node of addedNodes) {
+                if (node.nodeType !== Node.ELEMENT_NODE) continue;
+                
+                node.querySelectorAll('select[name$="[recipeIngredient]"]').forEach(addDefaultsForSelect);
+            }
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
     });
 });
